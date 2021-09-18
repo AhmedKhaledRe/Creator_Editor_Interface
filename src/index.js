3985a070614145620f93a02675ddb4616a5202c8
Ahmed Khaled <ahmed11khaled93@gmail.com>
2021-09-18 13:41:06 +0200

    add routes and final structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./config/state/store";
 import reportWebVitals from './reportWebVitals';
+import CreateQuiz from "./common/component/CreateQuiz";
+import EditQuiz from "./common/component/EditQuiz";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/quiz/create" component={CreateQuiz} />
+          <Route exact path="/quiz/edit/:id" component={EditQuiz} />
+          <Route path="/" component={App} />
+        </Switch>
+      </Suspense>
     </Provider>
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
